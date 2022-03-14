e3532ec0525e9d4134ef18ffb098ab1c3f6d51de
mbaluev <mbaluev@pmoractice.ru>
2022-03-14 21:40:01 +0700

    core v 1.1

--- src/controller/infrastructure/container.ts
+++ src/controller/infrastructure/container.ts
@@ -3,6 +3,7 @@ import {ERRORS_MODULE} from '../errors';
 import {ApiModule} from './modules';
 import {INFRASTRUCTURE_MODULE} from './ids';
 import {IErrorsModule} from '../errors/modules/base/interface';
+import {REACT_APP_CORE_URL} from './const';
 
 export const infrastructureContainer = new Container();
 
@@ -10,6 +11,5 @@ infrastructureContainer
   .bind(INFRASTRUCTURE_MODULE.Core)
   .toDynamicValue(({container}) => {
     const errorsContainer = container.get<IErrorsModule>(ERRORS_MODULE);
-    const apiBase = process.env.REACT_APP_CORE_URL || 'unknown/';
-    return new ApiModule(apiBase, errorsContainer);
+    return new ApiModule(REACT_APP_CORE_URL, errorsContainer);
   });
